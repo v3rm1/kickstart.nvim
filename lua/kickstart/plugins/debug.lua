be460b5e2bcc6fb4e34722b343f89657e8ad954d
v3rm1_UiA <varun.ravi.varma@uia.no>
2024-04-05 10:22:07 +0200

    Updated debug dap settings

--- lua/kickstart/plugins/debug.lua
+++ lua/kickstart/plugins/debug.lua
@@ -22,10 +22,27 @@ return {
     -- Add your own debuggers here
     'leoluz/nvim-dap-go',
     'microsoft/debugpy',
+    'mfussenegger/nvim-dap-python',
   },
   config = function()
     local dap = require 'dap'
     local dapui = require 'dapui'
+    dap.adapters.python = {
+      type = 'debug';
+      command = 'python';
+      args = { '-m', 'debugpy.adapter' };
+    }
+    dap.configurations.python = {
+      {
+        type = 'python';
+        request = 'launch';
+        name = "Launch file";
+        program = "${file}";
+        pythonPath = function()
+          return 'python'
+        end;
+      },
+    }
 
     require('mason-nvim-dap').setup {
       -- Makes a best effort to setup the various debuggers with
@@ -40,7 +57,7 @@ return {
       -- online, please don't ask me how to install them :)
       ensure_installed = {
         -- Update this to ensure that you have the debuggers for the langs you want
-        'delve',
+        'debugpy'
       },
     }
 
@@ -84,6 +101,7 @@ return {
     dap.listeners.before.event_exited['dapui_config'] = dapui.close
 
     -- Install golang specific config
-    require('dap-go').setup()
+    -- require('dap-go').setup()
+    -- require('dap-python').setup()
   end,
 }
