786651206148a79787562eb6eb9685e7a744c661
v3rm1_UiA <varun.ravi.varma@uia.no>
2024-06-13 02:32:23 +0200

    Fixed telescope theme

--- init.lua
+++ init.lua
@@ -785,7 +785,8 @@ require('lazy').setup({
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`
     --'folke/tokyonight.nvim',
-    'catppuccin-mocha',
+    'catppuccin/nvim', 
+    name = "catppuccin",
     lazy = false, -- make sure we load this during startup if it is your main colorscheme
     priority = 1000, -- make sure to load this before all the other start plugins
     opts = {
