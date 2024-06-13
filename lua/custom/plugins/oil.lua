93d2df41554049ca904d88fa73d436a6e5d04089
v3rm1_UiA <varun.ravi.varma@uia.no>
2024-06-13 02:17:34 +0200

    Added oil filesystem editor

---
+++ lua/custom/plugins/oil.lua
@@ -0,0 +1,10 @@
+-- Edit filesystem like it's a buffer.
+
+return {
+    "stevearc/oil.nvim",
+    config = function()
+      local oil = require("oil")
+      oil.setup()
+      vim.keymap.set("n", "-", oil.toggle_float, {})
+    end,
+  }
