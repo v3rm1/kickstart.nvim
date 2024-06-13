ad378eddd2c9061ba554ec7f43d8133a97591799
v3rm1_UiA <varun.ravi.varma@uia.no>
2024-06-13 02:17:19 +0200

    fixed telescope theme issue

--- init.lua
+++ init.lua
@@ -785,7 +785,7 @@ require('lazy').setup({
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`
     --'folke/tokyonight.nvim',
-    -- 'shatur/neovim-ayu',
+    'catppuccin-mocha',
     lazy = false, -- make sure we load this during startup if it is your main colorscheme
     priority = 1000, -- make sure to load this before all the other start plugins
     opts = {
