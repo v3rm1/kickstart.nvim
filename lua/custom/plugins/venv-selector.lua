7e3e72fe75a8c6b03f386f8ab71030afbde3614f
v3rm1_UiA <varun.ravi.varma@uia.no>
2024-06-21 00:42:13 +0200

    Updated plugins and restructured.

--- lua/custom/plugins/venv-selector.lua
+++ lua/custom/plugins/venv-selector.lua
@@ -5,22 +5,30 @@ return {
   dependencies = { 'neovim/nvim-lspconfig', 'nvim-telescope/telescope.nvim', 'mfussenegger/nvim-dap', 'mfussenegger/nvim-dap-python', 'microsoft/debugpy' },
   opts = {
     -- Your options go here
-    name = { 'venv', '.venv'},
+    name = { 'venv', '.venv' },
     auto_refresh = true,
     dap_enabled = true,
   },
   event = 'VeryLazy', -- Optional: needed only if you want to type `:VenvSelect` without a keymapping
+  branch = 'regexp',
   keys = {
     -- Keymap to open VenvSelector to pick a venv.
     { '<leader>vs', '<cmd>VenvSelect<cr>' },
     -- Keymap to retrieve the venv from a cache (the one previously used for the same project directory).
-    { '<leader>vc', '<cmd>VenvSelectCached<cr>' },
+    -- { '<leader>vc', '<cmd>VenvSelectCached<cr>' },
   },
   config = function()
     require('venv-selector').setup {
-      anaconda_base_path = '/opt/homebrew/Caskroom/miniconda/base',
-      anaconda_envs_path = '/opt/homebrew/Caskroom/miniconda/base/envs',
+      -- anaconda_base_path = '/opt/homebrew/Caskroom/miniconda/base',
+      -- anaconda_envs_path = '/opt/homebrew/Caskroom/miniconda/base/envs',
+      settings = {
+        search = {
+          my_venvs = {
+            command = 'find python$ /opt/homebrew/Caskroom/miniconda/base/envs/',
+          },
+        },
+      },
     }
-    require('venv-selector').retrieve_from_cache()
+    -- require('venv-selector').retrieve_from_cache()
   end,
 }
